e9adad62623cf72989a47b2a5daf9f2f78889a3b
HJX-exoskeleton <2545304960@qq.com>
2025-10-20 13:50:03 +0800

    update

--- arduino_code/MatrixArray.ino
+++ arduino_code/MatrixArray.ino
@@ -1,111 +1,100 @@
-#define BAUD_RATE                 2000000
-#define ROW_COUNT                 16
-#define COLUMN_COUNT              16
+#define SCAN_RATE                 115200
+#define MATRIX_SIZE               15
 
-#define PIN_ADC_INPUT             A0
-#define PIN_SHIFT_REGISTER_DATA   2
-#define PIN_SHIFT_REGISTER_CLOCK  3
-#define PIN_MUX_CHANNEL_0         4  
-#define PIN_MUX_CHANNEL_1         5
-#define PIN_MUX_CHANNEL_2         6
-#define PIN_MUX_CHANNEL_3         7
-#define PIN_MUX_INHIBIT_0         8
+#define SENSOR_PIN                A0
+#define SR_DATA_PIN               2
+#define SR_CLOCK_PIN              3
+#define MUX_S0                    4
+#define MUX_S1                    5
+#define MUX_S2                    6
+#define MUX_S3                    7
+#define MUX_ENABLE                8
 
-#define SET_SR_DATA_HIGH()        PORTD|=B00000100
-#define SET_SR_DATA_LOW()         PORTD&=~B00000100
-#define SET_SR_CLK_HIGH()         PORTD|=B00001000
-#define SET_SR_CLK_LOW()          PORTD&=~B00001000
+// 快速端口操作
+#define SR_DATA_HIGH()            PORTD |= (1<<2)
+#define SR_DATA_LOW()             PORTD &= ~(1<<2)
+#define SR_CLOCK_HIGH()           PORTD |= (1<<3)
+#define SR_CLOCK_LOW()            PORTD &= ~(1<<3)
 
+void setup() {
+  Serial.begin(SCAN_RATE);
 
-#define ROWS_PER_MUX              16
-#define MUX_COUNT                 1
-#define CHANNEL_PINS_PER_MUX      4
+  // 配置引脚
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(SR_DATA_PIN, OUTPUT);
+  pinMode(SR_CLOCK_PIN, OUTPUT);
+  pinMode(MUX_S0, OUTPUT);
+  pinMode(MUX_S1, OUTPUT);
+  pinMode(MUX_S2, OUTPUT);
+  pinMode(MUX_S3, OUTPUT);
+  pinMode(MUX_ENABLE, OUTPUT);
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
+  // 启用第一个多路复用器
+  digitalWrite(MUX_ENABLE, LOW);
 
-int current_enabled_mux = MUX_COUNT - 1;  //init to number of last mux so enabled mux increments to first mux on first scan.
-
-void setup()
-{
-  Serial.begin(BAUD_RATE);
-  pinMode(PIN_ADC_INPUT, INPUT);
-  pinMode(PIN_SHIFT_REGISTER_DATA, OUTPUT);
-  pinMode(PIN_SHIFT_REGISTER_CLOCK, OUTPUT);
-  pinMode(PIN_MUX_CHANNEL_0, OUTPUT);
-  pinMode(PIN_MUX_CHANNEL_1, OUTPUT);
-  pinMode(PIN_MUX_CHANNEL_2, OUTPUT);
-  pinMode(PIN_MUX_CHANNEL_3, OUTPUT);
-  pinMode(PIN_MUX_INHIBIT_0, OUTPUT);
-  // pinMode(PIN_MUX_INHIBIT_1, OUTPUT);
-
-  sbi(ADCSRA,ADPS2);  //set ADC prescaler to CLK/16
-  cbi(ADCSRA,ADPS1);
-  cbi(ADCSRA,ADPS0);
+  // 提高ADC速度
+  ADCSRA = (ADCSRA & 0xF8) | 0x04; // 16分频
 }
 
-void loop()
-{
-  for(int i = 0; i < ROW_COUNT; i ++)
-  {
-    setRow(i);
-    shiftColumn(true);
-    shiftColumn(false);                    
-    for(int j = 0; j < COLUMN_COUNT; j ++)
-    {
-      int raw_reading = analogRead(PIN_ADC_INPUT);
-      byte send_reading = (byte) (lowByte(raw_reading >> 2));
-      shiftColumn(false);
-      Serial.print(send_reading);
-      Serial.print(" ");
+void loop() {
+  scanMatrix();
+  Serial.println("---"); // 帧分隔符
+  delay(10);
+}
+
+void scanMatrix() {
+  for (byte row = 0; row < MATRIX_SIZE; row++) {
+    selectRow(row);
+
+    // 重置列选择
+    resetColumns();
+
+    for (byte col = 0; col < MATRIX_SIZE; col++) {
+      // 选择当前列
+      advanceColumn();
+
+      // 读取并处理传感器数据
+      int sensorValue = analogRead(SENSOR_PIN);
+      byte processedValue = processReading(sensorValue);
+
+      Serial.print(processedValue);
+      if (col < MATRIX_SIZE - 1) Serial.print(",");
     }
     Serial.println();
   }
-  Serial.println();
-  // delay(20);
 }
 
+void selectRow(byte row) {
+  // 设置多路复用器通道
+  digitalWrite(MUX_S0, row & 0x01);
+  digitalWrite(MUX_S1, row & 0x02);
+  digitalWrite(MUX_S2, row & 0x04);
+  digitalWrite(MUX_S3, row & 0x08);
+}
 
-void setRow(int row_number)
-{
-  if((row_number % ROWS_PER_MUX) == 0) 
-  {
-    digitalWrite(PIN_MUX_INHIBIT_0 + current_enabled_mux, HIGH);  
-    current_enabled_mux ++;
-    if(current_enabled_mux >= MUX_COUNT)
-    {
-      current_enabled_mux = 0;
-    }
-    digitalWrite(PIN_MUX_INHIBIT_0 + current_enabled_mux, LOW); 
-  }
-  for(int i = 0; i < CHANNEL_PINS_PER_MUX; i ++)
-  {
-    if(bitRead(row_number, i))
-    {
-      digitalWrite(PIN_MUX_CHANNEL_0 + i, HIGH);
-    }
-    else
-    {
-      digitalWrite(PIN_MUX_CHANNEL_0 + i, LOW);
-    }
+void resetColumns() {
+  // 重置移位寄存器
+  SR_DATA_HIGH();
+  pulseClock();
+  SR_DATA_LOW();
+
+  // 填充剩余位
+  for (byte i = 0; i < MATRIX_SIZE; i++) {
+    pulseClock();
   }
 }
 
-void shiftColumn(boolean is_first)
-{
-  if(is_first)
-  {
-    SET_SR_DATA_HIGH();
-  }
-  SET_SR_CLK_HIGH();
-  SET_SR_CLK_LOW();
-  if(is_first)
-  {
-    SET_SR_DATA_LOW();
-  }
+void advanceColumn() {
+  pulseClock();
 }
 
+void pulseClock() {
+  SR_CLOCK_HIGH();
+  SR_CLOCK_LOW();
+}
+
+byte processReading(int raw) {
+  // 转换10位ADC到8位并应用阈值
+  byte value = (raw >> 2) & 0xFF;
+  return (value > 45) ? (value - 45) : 0;
+}
