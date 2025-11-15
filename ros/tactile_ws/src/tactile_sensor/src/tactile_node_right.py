511cf99161fbee45fa1dcd9e13f5b5cbcf859412
HJX-exoskeleton <2545304960@qq.com>
2025-11-15 22:05:45 +0800

    update

--- ros/tactile_ws/src/tactile_sensor/src/tactile_node_right.py
+++ ros/tactile_ws/src/tactile_sensor/src/tactile_node_right.py
@@ -4,9 +4,10 @@ import serial
 import numpy as np
 import cv2
 import time
-import rospy 
+import rospy
 import threading
 from std_msgs.msg import Float64MultiArray
+
 
 def temporal_filter(new_frame, prev_frame, alpha=1):
     """
@@ -16,11 +17,12 @@ def temporal_filter(new_frame, prev_frame, alpha=1):
     """
     return alpha * new_frame + (1 - alpha) * prev_frame
 
+
 def tactile_publisher(tactile_name, alpha=None):
     mean = np.zeros((16, 16))
     # Open the serial port
-    serDev = serial.Serial(f'{tactile_name}',1000000)
-    
+    serDev = serial.Serial(f'{tactile_name}', 1000000)
+
     serDev.flush()
 
     # Create a publisher object
@@ -30,9 +32,9 @@ def tactile_publisher(tactile_name, alpha=None):
     print("Start")
     data_tac = []
     num = 0
-    t1=0
+    t1 = 0
     backup = None
-    flag=False
+    flag = False
     current = None
     while True:
         if serDev.in_waiting > 0:
@@ -43,8 +45,8 @@ def tactile_publisher(tactile_name, alpha=None):
             if len(line) < 10:
                 if current is not None and len(current) == 16:
                     backup = np.array(current)
-                    print("fps",1/(time.time()-t1))
-                    t1 =time.time()
+                    print("fps", 1 / (time.time() - t1))
+                    t1 = time.time()
                     data_tac.append(backup)
                     num += 1
                     if num > 20:
@@ -55,14 +57,17 @@ def tactile_publisher(tactile_name, alpha=None):
                 str_values = line.split()
                 int_values = [int(val) for val in str_values]
                 matrix_row = int_values
-                current.append(matrix_row) 
+                current.append(matrix_row)
 
     data_tac = np.array(data_tac)
     median = np.median(data_tac, axis=0)
-    flag=True
+    flag = True
     print(f"{tactile_name} Finish Initialization!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     current = None
     prev_frame = np.zeros_like(median)
+
+    new = np.zeros((16, 16))  # 初始化新的帧数据
+
     while True:
         if serDev.in_waiting > 0:
             try:
@@ -72,14 +77,27 @@ def tactile_publisher(tactile_name, alpha=None):
             # print(len(line))
             if len(line) < 10:
                 if current is not None and len(current) == 16:
-                    backup = np.array(current)
+                    current_array = np.array(current)  # 将当前帧转为numpy数组
+                    temp = 0
+                    # 重新排列行数据，调整帧的顺序
+                    new[:8, :] = current_array[:8, :] + temp  # 前8行保持不变
+                    new[8, :] = current_array[15, :] + temp
+                    new[9, :] = current_array[14, :] + temp
+                    new[10, :] = current_array[13, :] + temp
+                    new[11, :] = current_array[12, :] + temp
+                    new[12, :] = current_array[11, :] + temp
+                    new[13, :] = current_array[10, :] + temp
+                    new[14, :] = current_array[9, :] + temp
+                    new[15, :] = current_array[8, :] + temp
+
+                backup = np.array(new)
                 current = []
                 if backup is not None:
-                    contact_data= backup-median-12
+                    contact_data = backup - median - 12
                     contact_data = np.clip(contact_data, 0, 100)
-                    
+
                     if np.max(contact_data) < 12:
-                        contact_data_norm = contact_data /100
+                        contact_data_norm = contact_data / 100
                     else:
                         contact_data_norm = contact_data / np.max(contact_data)
                     if alpha is not None:
@@ -96,16 +114,18 @@ def tactile_publisher(tactile_name, alpha=None):
                 str_values = line.split()
                 int_values = [int(val) for val in str_values]
                 matrix_row = int_values
-                current.append(matrix_row) 
+                current.append(matrix_row)
                 continue
+
 
 if __name__ == '__main__':
     rospy.init_node('tactile_publisher', anonymous=True)
     # tactile_reading('right_robot_left_finger')
-    right_robot_left_finger_Thread = threading.Thread(target=tactile_publisher, args=('right_robot_left_finger',0.5,))
+    right_robot_left_finger_Thread = threading.Thread(target=tactile_publisher, args=('right_robot_left_finger', 0.5,))
     right_robot_left_finger_Thread.daemon = True
     right_robot_left_finger_Thread.start()
-    right_robot_right_finger_Thread = threading.Thread(target=tactile_publisher, args=('right_robot_right_finger',0.5,))
+    right_robot_right_finger_Thread = threading.Thread(target=tactile_publisher,
+                                                       args=('right_robot_right_finger', 0.5,))
     right_robot_right_finger_Thread.daemon = True
     right_robot_right_finger_Thread.start()
     while not rospy.is_shutdown():
--- ros/tactile_ws/src/tactile_sensor/src/tactile_sensor.py
+++ ros/tactile_ws/src/tactile_sensor/src/tactile_sensor.py
@@ -63,6 +63,9 @@ def tactile_publisher(tactile_name, alpha=None):
     print(f"{tactile_name} Finish Initialization!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     current = None
     prev_frame = np.zeros_like(median)
+
+    new = np.zeros((16, 16))  # 初始化新的帧数据
+
     while True:
         if serDev.in_waiting > 0:
             try:
@@ -72,7 +75,20 @@ def tactile_publisher(tactile_name, alpha=None):
             # print(len(line))
             if len(line) < 10:
                 if current is not None and len(current) == 16:
-                    backup = np.array(current)
+                    current_array = np.array(current)  # 将当前帧转为numpy数组
+                    temp = 0
+                    # 重新排列行数据，调整帧的顺序
+                    new[:8, :] = current_array[:8, :] + temp  # 前8行保持不变
+                    new[8, :] = current_array[15, :] + temp
+                    new[9, :] = current_array[14, :] + temp
+                    new[10, :] = current_array[13, :] + temp
+                    new[11, :] = current_array[12, :] + temp
+                    new[12, :] = current_array[11, :] + temp
+                    new[13, :] = current_array[10, :] + temp
+                    new[14, :] = current_array[9, :] + temp
+                    new[15, :] = current_array[8, :] + temp
+
+                backup = np.array(new)
                 current = []
                 if backup is not None:
                     contact_data= backup-median-12
