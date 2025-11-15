1a5f2c7e12d80764cb40cfa70ef74cb0dddce7b5
HJX-exoskeleton <2545304960@qq.com>
2025-11-15 21:41:43 +0800

    update

---
+++ ros/tactile_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hjx/hjx_file/STF/STF_develop/ros/tactile_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hjx/hjx_file/URDF_model/catkin_ws/devel;/home/hjx/handsfree/handsfree_ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/hjx/hjx_file/URDF_model/catkin_ws/devel;/home/hjx/handsfree/handsfree_ros_ws/devel;/opt/ros/noetic' != '' else []
---
+++ ros/tactile_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/hjx/hjx_file/STF/STF_develop/ros/tactile_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
